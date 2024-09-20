cd5b006a2843c2d23767c9d235f7c66797e53c6f
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-09-20 00:12:29 -0500

    ignore error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import routes from '~react-pages'
 
 function App () {
