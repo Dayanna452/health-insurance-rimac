2e00134b84ddbec6b66201c655ac785bc6e60349
Fatima Dayanna Villanueva Flores <dayannavillanueva452@gmail.com>
2024-09-21 20:24:17 -0500

    feat: mobile stepers

---
+++ src/presentation/components/contents/PlansOptions/index.tsx
@@ -0,0 +1,12 @@
+import { IoChevronBackCircleOutline } from "react-icons/io5";
+
+export const PlansOptions = () => {
+  return (
+    <div className="container mt-40 px-100">
+      <div className="plans-option-back">
+        <IoChevronBackCircleOutline className="plans-option-back__icon" />
+        <span className="plans-option-back__text">Volver</span>
+      </div>
+    </div>
+  );
+};
