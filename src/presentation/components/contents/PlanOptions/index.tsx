6ed1836e2b2cc48c99f9698bf1d0ed7b4fc0c3c9
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-09-23 23:30:21 -0500

    feat: user dynamic name

--- src/presentation/components/contents/PlanOptions/index.tsx
+++ src/presentation/components/contents/PlanOptions/index.tsx
@@ -1,35 +1,37 @@
-import { useNavigate } from "react-router-dom";
-import { IoChevronBackCircleOutline } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom'
+import { IoChevronBackCircleOutline } from 'react-icons/io5'
 
-import { SelectPlan } from "../SelectPlan";
-import { Button } from "../../blocks/Button";
-import { Plans } from "./parts/Plans";
+import { SelectPlan } from '../SelectPlan'
+import { Button } from '../../blocks/Button'
+import { Plans } from './parts/Plans'
+import { useUserData } from '../../../../services/user'
 
 export const PlanOptions = () => {
-  const navigate = useNavigate();
-  const goBack = () => navigate(-1);
+  const navigate = useNavigate()
+  const { data } = useUserData()
+  const goBack = () => navigate(-1)
   return (
-    <section className="plan-options container">
-      <div className="plan-options-action">
+    <section className='plan-options container'>
+      <div className='plan-options-action'>
         <Button
           onClick={goBack}
-          variant="text"
-          color="purple-dark"
+          variant='text'
+          color='purple-dark'
           startIcon={<IoChevronBackCircleOutline />}
         >
           Volver
         </Button>
       </div>
-      <div className="plan-options-content">
-        <h3 className="plan-options-content__title mb-8">
-          Rocío ¿Para quién deseas cotizar?
+      <div className='plan-options-content'>
+        <h3 className='plan-options-content__title mb-8'>
+          {data?.name || 'Rocío'} ¿Para quién deseas cotizar?
         </h3>
-        <p className="plan-options-content__subtitle mb-32">
+        <p className='plan-options-content__subtitle mb-32'>
           Selecciona la opción que se ajuste más a tus necesidades.
         </p>
       </div>
       <SelectPlan />
       <Plans />
     </section>
-  );
-};
+  )
+}
