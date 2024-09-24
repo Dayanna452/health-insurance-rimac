f9983af409957e0c1c6f896b264e660c5dda68d4
Fatima Dayanna Villanueva Flores <dayannavillanueva452@gmail.com>
2024-09-24 16:10:59 -0500

    feat: useMediaQuery

--- src/presentation/components/blocks/PlanCard/index.tsx
+++ src/presentation/components/blocks/PlanCard/index.tsx
@@ -1,13 +1,13 @@
-import { FC, useMemo } from 'react'
-import { Button } from '../Button'
-import { HomeLight } from '../../icons/HomeLight'
+import { FC, useMemo } from "react";
+import { Button } from "../Button";
+import { HomeLight } from "../../icons/HomeLight";
 
 interface PlanCardProps {
-  title: string
-  price: number
-  discount?: boolean
-  descriptions: string[]
-  handleSelectPlan: () => void
+  title: string;
+  price: number;
+  discount?: boolean;
+  descriptions: string[];
+  handleSelectPlan: () => void;
 }
 
 export const PlanCard: FC<PlanCardProps> = ({
@@ -15,38 +15,38 @@ export const PlanCard: FC<PlanCardProps> = ({
   price,
   discount,
   descriptions,
-  handleSelectPlan
+  handleSelectPlan,
 }) => {
-  const newPrice = useMemo(() => price - price * 0.05, [price])
+  const newPrice = useMemo(() => price - price * 0.05, [price]);
 
   return (
-    <div className='plan-card d-flex flex-column'>
-      <div className='plan-card__header'>
-        <h3 className='header__title'>{title}</h3>
-        <i className='header__icon'>
+    <div className="plan-card d-flex flex-column">
+      <div className="plan-card__header">
+        <h3 className="header__title">{title}</h3>
+        <i className="header__icon">
           <HomeLight />
         </i>
       </div>
-      <h6 className='subheader__text'>COSTO DEL PLAN</h6>
+      <h6 className="subheader__text">COSTO DEL PLAN</h6>
       {discount ? (
-        <span className='subheader__discount my-4'>${price} antes</span>
+        <span className="subheader__discount my-4">${price} antes</span>
       ) : null}
-      <b className='subheader__price'>${discount ? newPrice : price} al mes</b>
-      <hr className='plan-card__divider' />
-      <ul className='plan-card__descriptions d-flex flex-column gap-24'>
+      <b className="subheader__price">${discount ? newPrice : price} al mes</b>
+      <hr className="plan-card__divider" />
+      <ul className="plan-card__descriptions d-flex flex-column gap-24">
         {descriptions.map((description, index) => (
           <li key={index}>{description}</li>
         ))}
       </ul>
       <Button
         fullWidth
-        color='primary'
-        variant='contained'
-        className='mt-auto'
+        color="primary"
+        variant="contained"
+        className="mt-auto"
         onClick={handleSelectPlan}
       >
         Seleccionar Plan
       </Button>
     </div>
-  )
-}
+  );
+};
--- src/presentation/components/contents/PlanOptions/parts/Plans.tsx
+++ src/presentation/components/contents/PlanOptions/parts/Plans.tsx
@@ -1,41 +1,47 @@
-import { EmblaOptionsType } from 'embla-carousel'
+import { EmblaOptionsType } from "embla-carousel";
 
-import { PlanCard } from '../../../blocks/PlanCard'
-import { Carousel } from '../../../blocks/Carousel'
-import { usePlansData } from '../../../../../services/plans'
-import { usePlan } from '../../../../../data/stores/usePlan'
-import { useNavigate } from 'react-router-dom'
+import { PlanCard } from "../../../blocks/PlanCard";
+import { Carousel } from "../../../blocks/Carousel";
+import { usePlansData } from "../../../../../services/plans";
+import { usePlan } from "../../../../../data/stores/usePlan";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "../../../../utils/useMediaQuery";
 
-const OPTIONS: EmblaOptionsType = {}
+const OPTIONS: EmblaOptionsType = {};
 
 export const Plans = () => {
   const navigate = useNavigate();
-  const { data } = usePlansData()
-  const plansData = data?.list ?? []
-  const { selectedForWhoPlan, setSelectedPlan } = usePlan()
+  const { data } = usePlansData();
+  const isTablet = useMediaQuery("(min-width: 768px)");
+  const plansData = data?.list ?? [];
+  const { selectedForWhoPlan, setSelectedPlan } = usePlan();
+
+  console.log(isTablet);
 
   const planSlides = plansData.map((plan, index) => (
-    <div key={`plan-item-${index}`} className='plan-item'>
+    <div key={`plan-item-${index}`} className="plan-item">
       <PlanCard
         price={plan.price}
         title={plan.name}
         descriptions={plan.description}
-        discount={selectedForWhoPlan === 'other'}
+        discount={selectedForWhoPlan === "other"}
         handleSelectPlan={() => {
-          setSelectedPlan( plan )
-          navigate('/resume')
-          console.log('Plan selected', plan)
+          setSelectedPlan(plan);
+          navigate("/resume");
+          console.log("Plan selected", plan);
         }}
       />
     </div>
-  ))
+  ));
 
   return (
-    <div className='plans'>
-      <div className='plans__carousel'>
-        <Carousel slides={planSlides} options={OPTIONS} />
-      </div>
-      <div className='plans__list'>{planSlides}</div>
+    <div className="plans">
+      {!isTablet && (
+        <div className="plans__carousel">
+          <Carousel slides={planSlides} options={OPTIONS} />
+        </div>
+      )}
+      {isTablet && <div className="plans__list">{planSlides}</div>}
     </div>
-  )
-}
+  );
+};
